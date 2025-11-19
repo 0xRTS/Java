cd3e2e433ec2da3f1a5a92d99551f95b61c2f635
Alessandro Mizzon <alessandro.mizzon@iisviolamarchesini.edu.it>
2025-11-19 11:41:15 +0100

    .

--- Lab6/es3.java
+++ Lab6/es3.java
@@ -1,5 +1,49 @@
 public class es3{
 	public static void main(String[] args){
+		int[] a = {1, 2, 3};
+		double[] b = {11, 22, 33};
+		
+		a = resize(a, 2);
+		b = resize(b, 4);
+		
+		for(int i=0; i<a.length; i++){
+			System.out.println("a[" + i + "]: " + a[i]);
+		}
+		
+		for(int i=0; i<b.length; i++){
+			System.out.println("b[" + i + "]: " + b[i]);
+		}
+	}
+	
+	public static int[] resize(int[] arr, int len){
+		int[] newArr = new int[len];
 
+		if(arr.length>len){
+			for(int i=0; i<len; i++){
+				newArr[i] = arr[i];
+			}
+		}else{			
+			for(int i=0; i<arr.length; i++){
+				newArr[i] = arr[i];
+			}			
+		}
+		
+		return newArr;
+	}
+	
+	public static double[] resize(double[] arr, int len){
+		double[] newArr = new double[len];
+
+		if(arr.length>len){
+			for(int i=0; i<len; i++){
+				newArr[i] = arr[i];
+			}
+		}else{			
+			for(int i=0; i<arr.length; i++){
+				newArr[i] = arr[i];
+			}			
+		}
+		
+		return newArr;
 	}
 }
